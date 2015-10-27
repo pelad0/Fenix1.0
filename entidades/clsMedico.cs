ecb6d8c5be06e8733e3862ae17ab964ee2b894d5
pelad0 <latintainc@gmail.com>
2015-10-27 19:36:36 -0300

    clsMedico

--- entidades/clsMedico.cs
+++ entidades/clsMedico.cs
@@ -8,5 +8,69 @@ namespace entidades
 {
     public class clsMedico : IEntidad
     {
+        int idMedico;
+        int matricula;
+        long dni;
+        string nombre;
+        string apellido;
+        string especialidad;
+
+        public clsMedico(int id, int matricula, long dni, string nombre, string apellido, string especialidad)
+        {
+            this.idMedico = id;
+            this.matricula = matricula;
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.especialidad = especialidad;
+        }
+
+        public clsMedico(int matricula, long dni, string nombre, string apellido, string especialidad)
+        {
+            this.matricula = matricula;
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.especialidad = especialidad;
+        }
+
+        public int IdMedico
+        {
+            get { return idMedico; }
+            set { idMedico = value; }
+        }
+
+        public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public long Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+
+        public string Especialidad
+        {
+            get { return especialidad; }
+            set { especialidad = value; }
+        }
     }
 }
--- interfaces/IEntidad.cs
+++ interfaces/IEntidad.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace interfaces
 {
-    public class Class1
+    class IEntidad
     {
+        int Id { get; set; }
     }
 }
