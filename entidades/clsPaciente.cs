99cbdc37672d422c06070fdc9694fdd1637a4fb8
pelad0 <latintainc@gmail.com>
2015-10-28 16:07:03 -0300

    entidad paciente

---
+++ entidades/clsPaciente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using interfaces;
+
+namespace entidades
+{
+    class clsPaciente : IEntidad
+    {
+        int id;
+        string nombre;
+        string apellido;
+        long dni;
+        string obraSocial;
+        long telefono;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public long Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        public string ObraSocial
+        {
+            get { return obraSocial; }
+            set { obraSocial = value; }
+        }
+
+        public long Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+    }
+}
