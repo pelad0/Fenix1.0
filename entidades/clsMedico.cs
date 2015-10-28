03962b24e8e90fd557229bb0b678b284af799656
pelad0 <latintainc@gmail.com>
2015-10-28 17:07:32 -0300

    ent

--- entidades/clsMedico.cs
+++ entidades/clsMedico.cs
@@ -16,8 +16,9 @@ namespace entidades
         string apellido;
         string especialidad;
 
-        public clsMedico(int matricula, long dni, string nombre, string apellido, string especialidad)
+        public clsMedico(int id, int matricula, long dni, string nombre, string apellido, string especialidad)
         {
+            this.idMedico = id;
             this.matricula = matricula;
             this.dni = dni;
             this.nombre = nombre;
