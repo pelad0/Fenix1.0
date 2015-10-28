ebe3005268c1a7e1ec7efbda8b118c0ee500cf0c
pelad0 <latintainc@gmail.com>
2015-10-28 16:27:03 -0300

    TextBoxNumero

---
+++ controles/TextBoxNumeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace controles
+{
+    class TextBoxNumeros : TextBox
+    {
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+
+            if (char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
