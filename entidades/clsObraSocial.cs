87c773d9a98e1759f1db1011717cfe2748a15826
pelad0 <latintainc@gmail.com>
2015-10-28 16:01:59 -0300

    entidad Obra Social

---
+++ entidades/clsObraSocial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using interfaces;
+
+namespace entidades
+{
+    class clsObraSocial : IEntidad
+    {
+        int id;
+
+        string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }    
+    }
+}
