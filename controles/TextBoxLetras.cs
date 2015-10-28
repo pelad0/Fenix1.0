8837a525e723feae29371dc73c2498480526f330
pelad0 <latintainc@gmail.com>
2015-10-28 16:26:41 -0300

    TextBoxLetras

---
+++ controles/TextBoxLetras.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace controles
+{
+    class TextBoxLetras : TextBox
+    {
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+
+            if (char.IsNumber(e.KeyChar) || char.IsSymbol(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.KeyChar = (e.KeyChar.ToString()).ToUpper().ToCharArray()[0];
+            }
+        }
+    }
+}
