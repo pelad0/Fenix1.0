d81853cee1e3b6f0e639e76a188edd9f8b14b4e9
Wizzac <sebycuk@yahoo.com.ar>
2015-10-29 11:12:00 -0300

    commit interfaces

--- interfaces/iMetodos.cs
+++ interfaces/iMetodos.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using interfaces;
+
 using System.Data;
 
 namespace interfaces
 {
-   public interface iMetodos : IEntidad
+   public interface iMetodos 
     {
         void Alta(IEntidad entidad);
         void Baja(IEntidad entidad);
