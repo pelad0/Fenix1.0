7b2ea4298f089740efd806979040d9b5e19419d4
Wizzac <sebycuk@yahoo.com.ar>
2015-10-29 18:08:57 -0300

    Implemente bd metodos en la capa de datos junto al manejamedico, retoque un par de cosas tambien en el constructor de clsmedico

--- Datos/ManejaMedico.cs
+++ Datos/ManejaMedico.cs
@@ -5,49 +5,105 @@ using System.Text;
 using System.Threading.Tasks;
 using interfaces;
 using entidades;
+using System.Data;
 
 namespace Datos
 {
     public class ManejaMedico : iMetodos
     {
+        iBdMetodos manager;
 
-
+        public ManejaMedico()
+        {
+            manager=new BdMetodos();
+        }
 
         public void Alta(IEntidad entidad)
         {
-            throw new NotImplementedException();
+            clsMedico med = (clsMedico)entidad;
+            try
+            {
+                manager.ejecutar("Insert into medico(nombre,apellido,dni,especialidad,matricula) values("+med.Nombre+","+med.Apellido+","+med.Dni+","+med.Especialidad+","+med.Matricula+")");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+            }
         }
 
         public void Baja(IEntidad entidad)
         {
-            throw new NotImplementedException();
+            clsMedico med = (clsMedico)entidad;
+            try
+            {
+                manager.ejecutar("update medico set activo=0 where id=" + med.Id);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public void Modificacion(IEntidad entidad)
         {
-            throw new NotImplementedException();
+            clsMedico med = (clsMedico)entidad;
+            try
+            {
+                manager.ejecutar("update medico set nombre="+med.Nombre+",apellido="+med.Apellido+",dni="+med.Dni+",especialidad="+med.Especialidad+",matricula="+med.Matricula+ "where id=" + med.Id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IEntidad buscaPorId(int id)
         {
-            throw new NotImplementedException();
+            DataTable aux = new DataTable();
+            clsMedico med = new clsMedico();
+            try
+            {
+               aux=manager.consultar("select * from medico where idmedico="+id+"and activo=1");
+                med.Id = Convert.ToInt32(aux.Rows[0]["idPaciente"]);
+                med.Nombre = aux.Rows[0]["nombre"].ToString();
+                med.Apellido = aux.Rows[0]["apellido"].ToString();
+                med.Dni = Convert.ToInt64(aux.Rows[0]["dni"]);
+                med.Matricula = Convert.ToInt32(aux.Rows[0]["matricula"]);
+                med.Especialidad.Add((aux.Rows[0]["especialidad"].ToString()));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            /*
+             
+            clsPaciente P = new clsPaciente();
+            try
+            {
+                aux =manejar.Consultar("select * from paciente where idPaciente="+id);
+                */
+            return med;
         }
 
-        public System.Data.DataTable Todo(int pagina)
+        public DataTable Todo(int pagina)
         {
-            throw new NotImplementedException();
+            DataTable aux = new DataTable();
+            try
+            {
+                aux = manager.consultar("select * from medico where activo=1");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return aux;
         }
 
-        public int Id
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
+   
     }
 }
