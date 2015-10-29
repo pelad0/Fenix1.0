3968f54c073019432b9746dabbfebcaa09c6b0f7
pelad0 <latintainc@gmail.com>
2015-10-29 13:22:39 -0300

    frmABMUsuario

---
+++ Fenix1.0/frmABMUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using controles;
+
+
+namespace Fenix1._0
+{
+    public partial class frmABMUsuario : Form
+    {
+        public frmABMUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAlta_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
