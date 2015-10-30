a87bafcd4206c8e484ddedbe41e0324dc82b91a2
mariano <benitezmariano5@gmail.com>
2015-10-30 12:12:30 -0300

    logia

---
+++ logica/RepositorioMedico.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using interfaces;
+using entidades;
+using Datos;
+
+namespace logica
+{
+    public class RepositorioMedico
+    {
+        public void Alta(IEntidad medico)
+        {
+            ManejaMedico manejaMedico = new ManejaMedico();
+            manejaMedico.Alta(medico);
+        }
+
+        public void Baja(IEntidad medico)
+        {
+            ManejaMedico manejaMedico = new ManejaMedico();
+            manejaMedico.Baja(medico);
+        }
+
+
+        public void Modificacion(IEntidad medico)
+        {
+            ManejaMedico manejaMedico = new ManejaMedico();
+            manejaMedico.Modificacion(medico);
+        }
+
+        public clsMedico buscarPorId(int id)
+        {
+            clsMedico med = new clsMedico();
+            ManejaMedico manejaMedico = new ManejaMedico();
+            med = (clsMedico)manejaMedico.buscaPorId(id);
+
+            return med;
+        }
+
+    }
+}
