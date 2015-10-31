161be190ec74c3492e302c0ae6fb5c1a21a38db4
mariano <benitezmariano5@gmail.com>
2015-10-31 12:02:14 -0300

    dudas

---
+++ logica/RepositorioEspecialidad.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using interfaces;
+using entidades;
+using Datos;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace logica
+{
+    public class RepositorioEspecialidad
+    {
+
+        ManejaEspecialidad manejaEspecialidad = new ManejaEspecialidad();
+        public void Alta(IEntidad especialidad)
+        {
+                        
+            try
+            {
+                manejaEspecialidad.Alta(especialidad);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public void Baja(IEntidad especialidad)
+        {
+            try
+            {
+                                
+                manejaEspecialidad.Baja(especialidad);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+
+        public void Modificacion(IEntidad especialidad)
+        {
+            try
+            {
+               manejaEspecialidad.Modificacion(especialidad);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+        public clsEspecialidad buscarPorId(int id)
+        {
+            clsEspecialidad esp = new clsEspecialidad();
+            try
+            {
+                
+                esp = (clsEspecialidad)manejaEspecialidad.buscaPorId(id);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return esp;
+        }
+
+        public List<clsEspecialidad> Todo(int pag)
+        {
+            DataTable tabla;
+            List<clsEspecialidad> lista = new List<clsEspecialidad>();
+           
+            tabla = manejaEspecialidad.Todo(pag);
+            foreach (DataRow aux in tabla.Rows)
+            {
+                clsEspecialidad esp = new clsEspecialidad();
+                esp.Id = Convert.ToInt32(aux["id"].ToString());
+                esp.Descripcion = aux["descripcion"].ToString();
+                lista.Add(esp);
+            }
+
+            return lista;
+
+        }
+    }
+}
