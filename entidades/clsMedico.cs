324cc360180aab414131a63e6edc16dc7377f9bc
Wizzac <sebycuk@yahoo.com.ar>
2015-10-31 16:44:07 -0300

    Arregle otra vez las entidades

--- entidades/clsMedico.cs
+++ entidades/clsMedico.cs
@@ -14,11 +14,17 @@ namespace entidades
         long dni;
         string nombre;
         string apellido;
-        string especialidad;
+        int especialidad;
+
+        public int Especialidad
+        {
+            get { return especialidad; }
+            set { especialidad = value; }
+        }
 
         List<string> obraSocial;
 
-        public clsMedico(int id, int matricula, long dni, string nombre, string apellido, string especialidad, List<string> obraSocial )
+        public clsMedico(int id, int matricula, long dni, string nombre, string apellido, int especialidad, List<string> obraSocial )
         {
             this.id = id;
             this.matricula = matricula;
@@ -29,7 +35,7 @@ namespace entidades
             this.obraSocial = obraSocial;
         }
 
-        public clsMedico(int matricula, long dni, string nombre, string apellido, string especialidad, List<string> obraSocial)
+        public clsMedico(int matricula, long dni, string nombre, string apellido, int especialidad, List<string> obraSocial)
         {
             this.matricula = matricula;
             this.dni = dni;
@@ -76,11 +82,6 @@ namespace entidades
         }
 
 
-        public string Especialidad
-        {
-            get { return especialidad; }
-            set { especialidad = value; }
-        }
 
     
 
