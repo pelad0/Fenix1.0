5d3026fe2fccb1c2eb2a54a1e51b4c978faf0aa0
Wizzac <sebycuk@yahoo.com.ar>
2015-10-31 10:34:22 -0300

    ahora si clsmaneja paciente

--- Datos/ManejaPaciente.cs
+++ Datos/ManejaPaciente.cs
@@ -76,7 +76,7 @@ namespace Datos
                 pac.Nombre = aux.Rows[0]["nombre"].ToString();
                 pac.Apellido = aux.Rows[0]["apellido"].ToString();
                 pac.Dni = Convert.ToInt64(aux.Rows[0]["dni"]);
-                pac.ObraSocial = aux.Rows[0]["obra"];
+                pac.ObraSocial = Convert.ToInt32(aux.Rows[0]["obra"]);
                 pac.Telefono = Convert.ToInt32(aux.Rows[0]["matricula"]);
             }
             catch (Exception ex)
