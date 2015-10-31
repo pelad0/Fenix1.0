039d55925b56bef7a2647b3f8620e7918fa6b886
mariano <benitezmariano5@gmail.com>
2015-10-31 19:14:14 -0300

    logica1.0

---
+++ logica/RepositorioUsuario.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using interfaces;
+using entidades;
+using Datos;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace logica
+{
+    public class RepositorioUsuario
+    {
+        ManejaUsuario manejaUsuario = new ManejaUsuario();
+        public void Alta(IEntidad usuario)
+        {
+            try
+            {
+                manejaUsuario.Alta(usuario);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Baja(IEntidad usuario)
+        {
+            try
+            {
+                manejaUsuario.Baja(usuario);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Modificacion(IEntidad usuario)
+        {
+            try
+            {
+                manejaUsuario.Modicacion(usuario);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public clsUsuario buscarPorId(int id)
+        {
+            clsUsuario usu = new clsUsuario();
+            try
+            {
+                usu=manejaUsuario.buscarPorId(id);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return usu;
+        }
+
+        public List<clsUsuario> Todo(int pag)
+        {
+            DataTable tabla;
+            List<clsUsuario> lista = new List<clsUsuario>();
+           
+            tabla = manejaUsuario.Todo(pag);
+            foreach (DataRow i in tabla.Rows)
+            {
+
+                clsMedico med = new clsMedico();
+
+            }
+
+            return lista;
+
+        }
+
+    }
+}
