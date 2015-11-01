7cfdcfbee0f6aa86926ceb728f9617f97e009aa5
Wizzac <sebycuk@yahoo.com.ar>
2015-11-01 08:08:20 -0300

    manejador historial

--- Datos/ManejaHistorial.cs
+++ Datos/ManejaHistorial.cs
@@ -3,47 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using interfaces;
 using System.Text;
+using entidades;
+using interfaces;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace Datos
 {
     public class ManejaHistorial : iMetodos
     {
+        iBdMetodos manager;
+
+        public ManejaHistorial()
+        {
+            manager = new BdMetodos();
+        }
         public void Alta(IEntidad entidad)
         {
-            throw new NotImplementedException();
+            clsHistorial hist = (clsHistorial)entidad;
+            try
+            {
+                manager.ejecutar("Insert into historial(Id,diagnostico,observaciones,fecha) values('" + hist.Id1 + "','" + hist.Diagnostico + "'," + hist.Observaciones + "," + hist.Fecha +");SELECT @@identity;");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public void Baja(IEntidad entidad)
         {
-            throw new NotImplementedException();
+            clsHistorial hist = (clsHistorial)entidad;
+            try
+            {
+                manager.ejecutar("update historial set activo=0 where id=" + hist.Id1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public void Modificacion(IEntidad entidad)
         {
-            throw new NotImplementedException();
+            clsHistorial hist = (clsHistorial)entidad;
+            try
+            {
+                manager.ejecutar("update historial set diagnostico=" + hist.Diagnostico + ",observaciones=" + hist.Observaciones + ",fecha=" + hist.Fecha + "where id=" + hist.Id1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IEntidad buscaPorId(int id)
         {
-            throw new NotImplementedException();
+            DataTable aux = new DataTable();
+            clsHistorial hist = new clsHistorial();
+            try
+            {
+                aux = manager.consultar("select * from historial where Id=" + id + " and activo=1");
+                hist.Id1 = Convert.ToInt32(aux.Rows[0]["Id"]);
+                hist.Diagnostico = aux.Rows[0]["diagnostico"].ToString();
+                hist.Observaciones= aux.Rows[0]["observaciones"].ToString();
+                hist.Fecha = Convert.ToDateTime(aux.Rows[0]["fecha"]);
+               
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return hist;
         }
 
         public System.Data.DataTable Todo(int pagina)
         {
-            throw new NotImplementedException();
+            DataTable aux = new DataTable();
+            try
+            {
+                
+                aux = manager.consultar(" SELECT * FROM (SELECT *, ROW_NUMBER() OVER (ORDER BY ID) AS RowNum FROM historial) AS tabla WHERE activo=1 and  tabla.RowNum BETWEEN" + pagina * 10 + " AND " + (pagina * 10) + 10);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return aux;
         }
 
-        public int Id
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
+       
     }
 }
