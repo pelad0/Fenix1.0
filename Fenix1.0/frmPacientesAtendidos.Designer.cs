5142bc9bdfcf8656b62a183c08abf5fff793cf22
VicenteSolari <prokeso_yop@hotmail.com>
2015-11-05 18:52:11 -0300

    ff

---
+++ Fenix1.0/frmPacientesAtendidos.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace frmABMME
+{
+    partial class frmPacientesAtendidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBoxMedico = new System.Windows.Forms.CheckBox();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dtpHasta = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpDesde = new System.Windows.Forms.DateTimePicker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbMedico = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvPacientes = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkBoxMedico);
+            this.panel1.Controls.Add(this.btnFiltrar);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(14, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 492);
+            this.panel1.TabIndex = 1;
+            // 
+            // checkBoxMedico
+            // 
+            this.checkBoxMedico.AutoSize = true;
+            this.checkBoxMedico.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxMedico.Location = new System.Drawing.Point(47, 59);
+            this.checkBoxMedico.Name = "checkBoxMedico";
+            this.checkBoxMedico.Size = new System.Drawing.Size(129, 26);
+            this.checkBoxMedico.TabIndex = 6;
+            this.checkBoxMedico.Text = "Por médico";
+            this.checkBoxMedico.UseVisualStyleBackColor = true;
+            this.checkBoxMedico.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiltrar.Location = new System.Drawing.Point(67, 436);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(87, 28);
+            this.btnFiltrar.TabIndex = 4;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Fechas";
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.dtpHasta);
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.dtpDesde);
+            this.panel4.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel4.Location = new System.Drawing.Point(3, 200);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(223, 211);
+            this.panel4.TabIndex = 4;
+            // 
+            // dtpHasta
+            // 
+            this.dtpHasta.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dtpHasta.Location = new System.Drawing.Point(10, 157);
+            this.dtpHasta.Name = "dtpHasta";
+            this.dtpHasta.Size = new System.Drawing.Size(200, 23);
+            this.dtpHasta.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(81, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 22);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Hasta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(81, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 22);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Desde";
+            // 
+            // dtpDesde
+            // 
+            this.dtpDesde.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dtpDesde.Location = new System.Drawing.Point(10, 66);
+            this.dtpDesde.Name = "dtpDesde";
+            this.dtpDesde.Size = new System.Drawing.Size(200, 23);
+            this.dtpDesde.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.cbMedico);
+            this.panel2.Location = new System.Drawing.Point(4, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(224, 114);
+            this.panel2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Seleccione médico";
+            // 
+            // cbMedico
+            // 
+            this.cbMedico.Enabled = false;
+            this.cbMedico.FormattingEnabled = true;
+            this.cbMedico.Location = new System.Drawing.Point(42, 58);
+            this.cbMedico.Name = "cbMedico";
+            this.cbMedico.Size = new System.Drawing.Size(121, 23);
+            this.cbMedico.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filtrar pacientes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(281, 487);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(612, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "*Doble click sobre un paciente para ver todos los turnos del mismo.";
+            // 
+            // dgvPacientes
+            // 
+            this.dgvPacientes.AllowUserToAddRows = false;
+            this.dgvPacientes.AllowUserToDeleteRows = false;
+            this.dgvPacientes.AllowUserToResizeRows = false;
+            this.dgvPacientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPacientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPacientes.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvPacientes.Location = new System.Drawing.Point(285, 14);
+            this.dgvPacientes.MultiSelect = false;
+            this.dgvPacientes.Name = "dgvPacientes";
+            this.dgvPacientes.ReadOnly = true;
+            this.dgvPacientes.RowHeadersVisible = false;
+            this.dgvPacientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPacientes.Size = new System.Drawing.Size(608, 465);
+            this.dgvPacientes.TabIndex = 19;
+            // 
+            // frmPacientesAtendidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 541);
+            this.Controls.Add(this.dgvPacientes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Name = "frmPacientesAtendidos";
+            this.Text = "frmPacientesAtendidos";
+            this.Load += new System.EventHandler(this.frmPacientesAtendidos_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbMedico;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDesde;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DateTimePicker dtpHasta;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxMedico;
+        private System.Windows.Forms.DataGridView dgvPacientes;
+    }
+}
---
+++ Fenix1.0/frmPacientesAtendidos.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using logica;
+using entidades;
+
+
+namespace frmABMME
+{
+    public partial class frmPacientesAtendidos : Form
+    {
+
+
+        RepositorioMedico reposMedico = new RepositorioMedico();
+        RepositorioTurno reposTurno = new RepositorioTurno();
+        RepositorioPaciente reposPaciente = new RepositorioPaciente();
+        bool columnas = false;
+        List<clsTurno> listaTurnos = new List<clsTurno>();
+
+        public frmPacientesAtendidos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPacientesAtendidos_Load(object sender, EventArgs e)
+        {
+            checkBoxMedico.Checked = false;
+
+            if (!columnas)   //Para que se creeen una sola vez y no tener que borrarlas
+            {
+                CrearColumnas();
+            }
+
+
+
+            CargarMedicos();        //Carga todos los medicos al combo box;
+            EstablecerFechas();     //Establece las fechas en los dateTimepicker, la menor es la del primer turno, la mayor del ultimo.
+
+        }
+
+
+
+
+        public void CargarMedicos()         //CARGA TODOS LOS MEDICOS EN EL COMBO BOX.
+        {
+            List<clsMedico> listaMedicos = new List<clsMedico>();
+
+            listaMedicos = reposMedico.ObtenerMedicos();        //Metodo que me trae todos los medicos
+
+            foreach(clsMedico med in listaMedicos)
+            {
+                cbMedico.Items.Add(med.Apellido);
+            }
+
+
+
+        }
+
+
+        public void EstablecerFechas()
+        {
+
+            clsTurno primerTurno = new clsTurno();
+            clsTurno ultimoTurno = new clsTurno();
+
+            primerTurno = reposTurno.Primero();     //Me retorna el primer turno dado.
+            ultimoTurno =  reposTurno.Ultimo();     //Me retorna el ultimo turno dado.
+
+            dtpDesde.MinDate = primerTurno.Fecha;
+            dtpHasta.MaxDate = ultimoTurno.Fecha;
+
+            dtpDesde.Value = primerTurno.Fecha;
+            dtpHasta.Value = ultimoTurno.Fecha;
+
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+
+            //obtenerTurnoMedico(idMedico);     Me retorna todos los turnos de un medico determinado.
+
+            if(checkBoxMedico.Checked)
+            {
+                if(string.IsNullOrWhiteSpace(cbMedico.Text))
+                {
+                    MessageBox.Show("Debe seleccionar un medico para continuar", "¡Advertencia!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {                    
+
+                    //Borro todas las filas.
+                    if (dgvPacientes.Rows.Count > 0)
+                    {
+                        foreach (DataGridViewRow row in dgvPacientes.Rows)
+                        {
+                            dgvPacientes.Rows.Remove(row);
+                        }
+                    }
+
+
+
+                    clsMedico medicoSeleccionado = new clsMedico();
+
+                    List<clsMedico> listaMedicos = new List<clsMedico>();
+
+                    listaMedicos = reposMedico.ObtenerMedicos();            //Traigo todos los medicos
+
+                    foreach(clsMedico med in listaMedicos)
+                    {
+                        if (med.Apellido == cbMedico.Text)
+                            medicoSeleccionado = med;
+                    }                                                       //Encuentro el medico seleccionado y traigo todos sus turnos
+
+                    listaTurnos = reposTurno.obtenerTurnoMedico(medicoSeleccionado.Id);     //Traigo todos los turnos dados por ese medico
+
+
+                    foreach(clsTurno turn in listaTurnos)   //Recorro todos los turnos obteniendo el paciente de cada uno.
+                    {
+                        if(turn.Fecha > dtpDesde.Value && turn.Fecha < dtpHasta.Value)
+                        {
+                            clsPaciente pas = new clsPaciente();
+                            pas = reposPaciente.buscarPorId(turn.IdPaciente);
+                            
+                            if(pas != null)         //Pregunto por si el paciente fue dado de baja.
+                            {
+                                dgvPacientes.Rows.Add(pas.Id, pas.Apellido, pas.Dni, pas.ObraSocial, pas.Telefono);
+                                                            
+                            }
+
+                                
+                        }
+
+
+                    }
+
+
+
+                }
+            }
+            else                //QUE EL CHECK BOX DE MEDICO NO FUE SELECCIONADO
+            {
+
+                //ACA LLAMO METODO QUE ME RETORNA TODOS LOS TURNOS ENTRE DOS FECHAS Y DE ESOS TURNOS SACO LOS
+                // PACIENTES QUE VOY A MOSTRAR.
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBoxMedico.Checked)
+            {
+                cbMedico.Enabled = false;
+            }
+            else
+            {
+                cbMedico.Enabled = true;
+            }
+
+        }
+
+        public void CrearColumnas()
+        {
+
+            dgvPacientes.Columns.Add("id", "id");
+            dgvPacientes.Columns["id"].Visible = false;
+            dgvPacientes.Columns.Add("Nombre", "Nombre");
+            dgvPacientes.Columns.Add("Apellido", "Apellido");
+            dgvPacientes.Columns.Add("Dni", "Dni");
+            dgvPacientes.Columns.Add("ObraSocial", "ObraSocial");
+            dgvPacientes.Columns.Add("Telefono", "Telefono");
+            
+
+            columnas = true;
+
+        }
+
+
+
+    }
+}
