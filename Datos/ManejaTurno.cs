42ca4097882bf98d814683c69a6bfb18cccb2106
Wizzac <sebycuk@yahoo.com.ar>
2015-11-09 15:58:44 -0300

    agregado fecha a buscarturnoporpaciente

--- Datos/ManejaTurno.cs
+++ Datos/ManejaTurno.cs
@@ -126,12 +126,12 @@ namespace Datos
 
             return aux;
         }
-        public DataTable BuscarTurnoPorPaciente(int id)
+        public DataTable BuscarTurnoPorPaciente(int id,DateTime fecha)
         {
             DataTable aux = new DataTable();
             try
             {
-                aux = manager.consultar("select * from turno where idpaciente=" + id + " and activo=1");
+                aux = manager.consultar("select * from turno where idpaciente=" + id + " and activo=1 and fecha='"+fecha+"'");
             }
             catch (Exception ex)
             {
