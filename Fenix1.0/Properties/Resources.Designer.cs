057694363abddabd4a13706c9ec12543c42e1e5b
VicenteSolari <prokeso_yop@hotmail.com>
2015-11-17 13:32:21 -0300

    sdfsdf

--- Fenix1.0/Properties/Resources.Designer.cs
+++ Fenix1.0/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace frmABMME.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fondo {
+            get {
+                object obj = ResourceManager.GetObject("fondo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
