a4ecf24e600180c247d989178c760f9dcdd8f317
VicenteSolari <prokeso_yop@hotmail.com>
2015-11-17 01:34:39 -0300

    fff

--- Datos/ManejaRecibo.cs
+++ Datos/ManejaRecibo.cs
@@ -22,8 +22,18 @@ namespace Datos
             clsRecibo rec = (clsRecibo)entidad;
             try
             {
-                //manager.ejecutar("Insert into recibo(idfactura,idsobreturno,idturno,fecha,cobertura,importe,detalle) values(" + rec.IdFactura + "," + rec.IdSobreTurno + "," + rec.IdTurno + ",'" + rec.Fecha + "'," + rec.Cobertura + "," + rec.Importe + ",'" + rec.Detalle + "');SELECT @@identity;");
-                manager.ejecutar("Insert into recibo(idfactura,idsobreturno,idturno,fecha,cobertura,importe,detalle) values(" + rec.IdFactura + "," + null + "," + rec.IdTurno + ",'" + rec.Fecha + "'," + 100 + "," + 200 + ",'" + rec.Detalle + "');SELECT @@identity;");
+               if(rec.IdSobreTurno == null)
+               {
+                   string hola;
+                   hola = DBNull.Value;
+                   manager.ejecutar("Insert into recibo(idfactura,idsobreturno,idturno,fecha,cobertura,importe,detalle) values(" + rec.IdFactura + "," + rec.IdSobreTurno + "," + rec.IdTurno + ",'" + rec.Fecha + "'," + rec.Cobertura + "," + rec.Importe + ",'" + rec.Detalle + "');SELECT @@identity;");
+
+               }
+               else
+               {
+
+               }
+                manager.ejecutar("Insert into recibo(idfactura,idsobreturno,idturno,fecha,cobertura,importe,detalle) values(" + rec.IdFactura + "," + rec.IdSobreTurno + "," + rec.IdTurno + ",'" + rec.Fecha + "'," + rec.Cobertura + "," + rec.Importe + ",'" + rec.Detalle + "');SELECT @@identity;");
 
             }
             catch (Exception ex)
